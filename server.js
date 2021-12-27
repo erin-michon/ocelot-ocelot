4f2b35146f7e7d3f264c3514fe1036b99e86a0d7
Erin M <erin.michon@gmail.com>
2021-12-26 21:51:31 -0500

    initial files and server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+//Express Middleware
+app.use(express.urlencoded({extended:false}));
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
