ea204f9fb9daf8d62f9a814643fe1eb7b1c8685f
Erin M <erin.michon@gmail.com>
2021-12-27 12:30:17 -0500

    added lib folder and .env files for db connection

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS emp_tracker_db;
+CREATE DATABASE emp_tracker_db;
+USE emp_tracker_db;
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+//Application Dependencies
 const inquirer = require('inquirer');
+const mysql = require("mysql2");
+const Department = require('./lib/Department');
 
+// Connect to database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'emp_tracker_db'
+  });
+
+
+//Initial Application Menu Options
 const appOptions = () => {
 
     console.log(`
@@ -15,7 +28,7 @@ const appOptions = () => {
             name: 'userAction',
             choices: ["View All Departments", "View All Roles", "View All Employees", 
             "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"],           
-            message: "Hello, and Welcome!  Please choose your action from the menu items below:"
+            message: "Hello! Please choose your action from the menu options below:"
         }
     ])
     .then(userOption => {
@@ -52,6 +65,41 @@ const appOptions = () => {
             console.log('Update an Employee Role')
             //Create a function that prompted to select an employee and then prompts the user for the role to update and their new role and this information in the database
         }; 
+        
+    });
+};
+
+//Prompts for Adding a Dept
+const addDept = () => {
+
+    console.log(`
+    ===================
+    Adding a Department
+    ===================
+    `);
+
+    return inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'deptName',
+            message: "Please enter the name of the new Department",
+        },
+    ])
+    .then(newDept => {
+        const {deptName} = newDept;
+        console.log()
+
+        if(userOption.userAction === 'View All Departments') {
+            console.log('View All Departments was selected')
+            //Show Table (Dept) with department names and department ids
+
+        } else if (userOption.userAction === 'View All Roles') {
+            console.log('View All Roles was selected')
+            //Show Table (Roles) with job title, role id, the department that role belongs to, 
+            //and the salary for that role
+        }
+            
         
     });
 };
