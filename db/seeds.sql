b130ac2e354b117c7787fded71f7519e386cd783
Erin M <erin.michon@gmail.com>
2022-01-23 21:10:53 -0500

    added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (name)
+  VALUES
+  ('Master Data Management'),
+  ('IM Clearing House'),
+  ('Plant Stores Supervisors');
+
+INSERT INTO role (title, salary, department_id)
+  VALUES
+  ('Manager', '55000.00', '1')
+  ('Manager', '55000.00', '2')
+  ('Work Chain Analyst','50000.00', '2'),
+  ('Spare Part Coordinator','50000.00', '1'),
+  ('Specification Gatekeeper','50000.00', '1'),
+  ('Catalog Gatekeeper','40000.00', '1'),
+  ('Catalog Analyst','55000.00', '1'),
+  ('MTO Coordinator','50000.00', '1')
+  ('Stores Supervisor','45000.00', '3')
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id )
+    VALUES
+    ('Erin', 'Carson', '1', NULL),
+    ('Cora', 'Peterson', '2', NULL),
+    ('Paulette', 'Stein', '1', '1'),
+    ('Bill', 'Brown', '3', '2'),
+    ('Tom', 'Arthur', '7', '1'),
+    ('Terry', 'Johnson', '4', '1'),
+    ('Brent', 'Gregory', '3', '1'),
+    ('Kevin', 'McConnell', '6', '1'),
+    ('Trey', 'Fishman', '5', '1'),
+    ('Mike', 'Gordon', '8', '1'),
+    ('Page', 'Farmhouse', '9', '3');
